e27c8c0019a410a51f742d7cfb940570dc5c39fd
Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
2024-01-02 05:11:00 +0700

    Setup json options and refactor code (#7)
    * Refactor host configurations
    
    Signed-off-by: Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
    
    * Setup json options and refactor code
    
    Signed-off-by: Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>

--- src/AnyMails.Api/Program.cs
+++ src/AnyMails.Api/Program.cs
@@ -1,50 +1,29 @@
-using AnyMails.Api;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Serilog;
-using Serilog.Exceptions;
+using AnyMails.Api.Extensions;
+using AnyMails.Application;
+using AnyMails.Infrastructure;
+using AutofacModule = AnyMails.Api.AutofacModule;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Host
-    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-    .ConfigureContainer<ContainerBuilder>(e => e.RegisterAssemblyModules(typeof(AutofacModule).Assembly))
-    .ConfigureAppConfiguration((context, config) =>
-    {
-        var environment = context.HostingEnvironment.EnvironmentName;
-        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        config.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
-        config.AddEnvironmentVariables();
+    .UseAutofacProviderFactory<AutofacModule>()
+    .UseCustomAppConfigurations()
+    .UseCustomSerilog();
 
-        // These are optional configuration files only apply to local
-        // DO NOT COMMIT THESE FILES!
-        // These files should already be in .gitignore file
-        config.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
-        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
-    })
-    .UseSerilog((context, services, config) =>
-    {
-        var seqHostName = context.Configuration["Seq:HostName"];
-        var seqApiKey = context.Configuration["Seq:ApiKey"];
-        if(!string.IsNullOrWhiteSpace(seqHostName) && !string.IsNullOrWhiteSpace(seqApiKey))
-        {
-            config.WriteTo.Seq(seqHostName, apiKey: seqApiKey);
-        }
-
-        config
-#if DEBUG
-            .Enrich.WithProperty("Environment", "Local")
-#else
-            .Enrich.WithProperty("Environment", context.Configuration["ASPNETCORE_ENVIRONMENT"])
-#endif
-            .Enrich.WithCorrelationId()
-            .Enrich.FromLogContext()
-            .Enrich.WithCorrelationId()
-            .Enrich.WithExceptionDetails()
-            .ReadFrom.Configuration(context.Configuration)
-            .ReadFrom.Services(services);
-    });
+builder.Services
+    .AddJsonOptions()
+    .Configure<RouteOptions>(options => { options.LowercaseUrls = true; })
+    .AddInfrastructure()
+    .AddApplication()
+    .AddHealthChecks();
 
 var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseServiceHealthChecks();
+
+app.MapReleaseEndpoint();
 
 app.MapGet("/", (ILogger logger) =>
 {
---
+++ src/AnyMails.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AnyMails.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
