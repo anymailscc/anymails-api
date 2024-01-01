4f88b2b696d178ef19c61ed0f20ce74160935a09
Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
2024-01-02 01:39:46 +0700

    Setup Autofac for DI framework

---
+++ src/AnyMails.Application/AutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Autofac;
+using Module = Autofac.Module;
+
+namespace AnyMails.Application;
+
+public class AutofacModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Default registration is by scope
+        builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces();
+    }
+}
