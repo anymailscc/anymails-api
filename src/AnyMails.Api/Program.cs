a2caf1bddfbcd6a07cdecb39c06f059e6ae36bd8
Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
2024-01-02 04:29:15 +0700

    Refactor host configurations
    Signed-off-by: Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>

--- src/AnyMails.Api/Program.cs
+++ src/AnyMails.Api/Program.cs
@@ -1,48 +1,12 @@
 using AnyMails.Api;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Serilog;
-using Serilog.Exceptions;
+using AnyMails.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Host
-    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-    .ConfigureContainer<ContainerBuilder>(e => e.RegisterAssemblyModules(typeof(AutofacModule).Assembly))
-    .ConfigureAppConfiguration((context, config) =>
-    {
-        var environment = context.HostingEnvironment.EnvironmentName;
-        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        config.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
-        config.AddEnvironmentVariables();
-
-        // These are optional configuration files only apply to local
-        // DO NOT COMMIT THESE FILES!
-        // These files should already be in .gitignore file
-        config.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
-        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
-    })
-    .UseSerilog((context, services, config) =>
-    {
-        var seqHostName = context.Configuration["Seq:HostName"];
-        var seqApiKey = context.Configuration["Seq:ApiKey"];
-        if(!string.IsNullOrWhiteSpace(seqHostName) && !string.IsNullOrWhiteSpace(seqApiKey))
-        {
-            config.WriteTo.Seq(seqHostName, apiKey: seqApiKey);
-        }
-
-        config
-#if DEBUG
-            .Enrich.WithProperty("Environment", "Local")
-#else
-            .Enrich.WithProperty("Environment", context.Configuration["ASPNETCORE_ENVIRONMENT"])
-#endif
-            .Enrich.WithCorrelationId()
-            .Enrich.FromLogContext()
-            .Enrich.WithCorrelationId()
-            .Enrich.WithExceptionDetails()
-            .ReadFrom.Configuration(context.Configuration)
-            .ReadFrom.Services(services);
-    });
+    .UseAutofacProviderFactory<AutofacModule>()
+    .UseCustomAppConfigurations()
+    .UseCustomSerilog();
 
 var app = builder.Build();
 
