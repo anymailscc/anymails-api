7911b2a1003fafaefa06bdf035545d7aa7c9390d
Taipan Prasithpongchai <9193983+taipan-pr@users.noreply.github.com>
2024-01-02 02:38:51 +0700

    Setup Serilog for logging

--- src/AnyMails.Api/Program.cs
+++ src/AnyMails.Api/Program.cs
@@ -1,14 +1,42 @@
 using AnyMails.Api;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-    .ConfigureContainer<ContainerBuilder>(e => e.RegisterAssemblyModules(typeof(AutofacModule).Assembly));
+    .ConfigureContainer<ContainerBuilder>(e => e.RegisterAssemblyModules(typeof(AutofacModule).Assembly))
+    .UseSerilog((context, services, config) =>
+    {
+        var seqHostName = context.Configuration["Seq:HostName"];
+        var seqApiKey = context.Configuration["Seq:ApiKey"];
+        if(!string.IsNullOrWhiteSpace(seqHostName) && !string.IsNullOrWhiteSpace(seqApiKey))
+        {
+            config.WriteTo.Seq(seqHostName, apiKey: seqApiKey);
+        }
+
+        config
+#if DEBUG
+            .Enrich.WithProperty("Environment", "Local")
+#else
+            .Enrich.WithProperty("Environment", context.Configuration["ASPNETCORE_ENVIRONMENT"])
+#endif
+            .Enrich.WithCorrelationId()
+            .Enrich.FromLogContext()
+            .Enrich.WithCorrelationId()
+            .Enrich.WithExceptionDetails()
+            .ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services);
+    });
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (ILogger logger) =>
+{
+    logger.Information("Hello world");
+    return "Hello World!";
+});
 
 app.Run();
